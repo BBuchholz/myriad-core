9b2cedf4f979abf8f0e51249599293e06ea94ac6
ENTOMDT <entomdt@gmail.com>
2021-08-13 13:14:52 -0500

    Add spec details for createWxrd from planning doc

---
+++ src/DjehutiController.spec.js
@@ -0,0 +1,28 @@
+/**
+ * @file Handles all xml import and export capabilities.
+ */
+
+const djehuti = require('./DjehutiController');
+const Wxrd = require('./Wxrd');
+
+test('should have at least one test', () => {
+
+  expect(true).toBe(true);
+});
+
+test('should create wxrd from multiline string input', ()=>{
+
+  const opRes = djehuti.createWxrd("Some text");
+  expect(opRes).toBeDefined();
+
+  // from planning doc:
+  // Should create a Wxrd as is currently done with 
+  // “Wxrd(initializationData)” but parsing from 
+  // stringInput as we originally intended (everything 
+  // roundtrips to the parser, so when features are turned off
+  // its just text, and when they get turned on, parts of that
+  // text takes on special significance)
+  
+  // Should return that Wxrd as the payload of an OperationResult
+
+});
