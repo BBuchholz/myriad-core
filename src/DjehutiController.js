16c5b77693ebffbf8e11f738f4d2f386ac0b4b2f
ENTOMDT <entomdt@gmail.com>
2021-08-14 11:39:05 -0500

    Update tests and linter

---
+++ src/DjehutiController.js
@@ -0,0 +1,39 @@
+/**
+ * @file Djehuti is the main controller for the Basecamp operations
+ * of Myriad.
+ */
+
+const Wxrd = require('./Wxrd');
+const WxrdBook = require('./WxrdBook');
+
+const DjehutiController = () => {
+
+  const self = {
+
+    createWxrd: (multiLineStringInput) => {
+
+      const newWxrd = Wxrd(multiLineStringInput);
+
+      const operationResult = {
+        payload: newWxrd,
+        payloadType: 'Wxrd',
+        successful: true,
+        messages: [],
+      };
+
+      return operationResult;
+
+    },
+
+    createWxrdBook: (bookTitle) => (
+
+      WxrdBook(bookTitle)
+
+    ),
+  };
+
+  return self;
+
+};
+
+module.exports = DjehutiController;
--- src/DjehutiController.spec.js
+++ src/DjehutiController.spec.js
@@ -2,38 +2,47 @@
  * @file Handles all xml import and export capabilities.
  */
 
-const djehuti = require('./DjehutiController');
-const Wxrd = require('./Wxrd');
+const DjehutiController = require('./DjehutiController');
+
+const djehuti = DjehutiController();
 
 test('should have at least one test', () => {
 
   expect(true).toBe(true);
+
 });
 
-test('should create wxrd from multiline string input', ()=>{
+test('should create wxrd from multiline string input', () => {
 
-  const opRes = djehuti.createWxrd("Some text");
+  const multiLineInput = `Something I wrote down to remember... 
+  alias: Test String
+  tags: testing, test`;
+
+  const opRes = djehuti.createWxrd(multiLineInput);
   expect(opRes).toBeDefined();
+  expect(opRes.payloadType).toBe('Wxrd');
+  expect(opRes.payload.getWxrdType()).toBe('Wxrd');
+  expect(opRes.successful).toBe(true);
+  expect(opRes.messages.length).toBe(0);
 
   // from planning doc:
-  // Should create a Wxrd as is currently done with 
-  // “Wxrd(initializationData)” but parsing from 
-  // stringInput as we originally intended (everything 
+  // Should create a Wxrd as is currently done with
+  // “Wxrd(initializationData)” but parsing from
+  // stringInput as we originally intended (everything
   // roundtrips to the parser, so when features are turned off
   // its just text, and when they get turned on, parts of that
   // text takes on special significance)
-  
+
   // Should return that Wxrd as the payload of an OperationResult
 
 });
 
-test('should create wxrdbook from input', ()=>{
+test('should create wxrdbook from input', () => {
 
-  const opRes = djehuti.createWxrdBook("some title", "some description");
+  const opRes = djehuti.createWxrdBook('some title');
   expect(opRes).toBeDefined();
 
   // Should return a new WxrdBook with the title and description set
   // Should return that WxrdBook as the payload of an OperationResult
-
 
 });
