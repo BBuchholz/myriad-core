04e976c973f395366dce55ef88429d9fcacb3981
ENTOMDT <entomdt@gmail.com>
2021-09-27 11:34:55 -0500

    Update index.js for v0.1.0

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const Wxrd = require('./src/Wxrd');
 const WxrdBook = require('./src/WxrdBook');
-const XmlTransport = require('./src/XmlTransport');     
+const XmlTransport = require('./src/XmlTransport'); 
+const DjehutiController = require('./src/DjehutiController');
+const MyceliaController = require('./src/MyceliaController');
+const AnansiController = require('./src/AnansiController');
+const ChappieController = require('./src/ChappieController');
+const AlexController = require('./src/AlexController');
+const PerduraboController = require('./src/PerduraboController');  
 
 module.exports = { 
 	Wxrd,
  	WxrdBook,
- 	XmlTransport
+ 	XmlTransport,
+ 	DjehutiController,
+	MyceliaController,
+	AnansiController,
+	ChappieController,
+	AlexController,
+	PerduraboController
 };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,14 @@
-const { Wxrd, WxrdBook, XmlTransport } = require('./index');
+const { 
+	Wxrd, 
+	WxrdBook, 
+	XmlTransport, 
+	PerduraboController, 
+	ChappieController,
+	AnansiController,
+	MyceliaController,
+	DjehutiController,
+	AlexController 
+} = require('./index');
 
 test('should have Wxrd defined in index', () => {
 
@@ -30,3 +40,40 @@ test('should have XmlTransport defined in index', () => {
 
 	expect(xmlString).toMatch('test');
 });
+
+it('should have DjehutiController defined in index', () => {
+
+	const testController = DjehutiController();
+	expect(testController).toBeDefined();
+});
+
+it('should have MyceliaController defined in index', () => {
+
+	const testController = MyceliaController();
+	expect(testController).toBeDefined();
+});
+
+
+it('should have AnansiController defined in index', () => {
+
+	const testController = AnansiController();
+	expect(testController).toBeDefined();
+});	
+	
+it('should have ChappieController defined in index', () => {
+
+	const testController = ChappieController();
+	expect(testController).toBeDefined();
+});	
+
+it('should have AlexController defined in index', () => {
+
+	const testController = AlexController();
+	expect(testController).toBeDefined();
+});	
+
+it('should have PerduraboController defined in index', () => {
+
+	const testController = PerduraboController();
+	expect(testController).toBeDefined();
+});	
